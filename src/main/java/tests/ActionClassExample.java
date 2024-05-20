d5ac42852d08083ca5ead2b1b8430f008e7fc640
unknown <irinaandrei83@gmail.com>
2024-05-20 11:26:51 +0300

    curs21 final

--- src/main/java/tests/ActionClassExample.java
+++ src/main/java/tests/ActionClassExample.java
@@ -1,7 +1,13 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import pages.ContactsPage;
 import pages.MenuPage;
 import pages.ShopPage;
 import utils.BaseTest;
@@ -22,7 +28,7 @@ public class ActionClassExample extends BaseTest {
 		
 	}
 
-	@Test
+	//@Test
 	public void dragAndDropExample() {
 		
 		MenuPage menu = new MenuPage(driver);
@@ -32,5 +38,31 @@ public class ActionClassExample extends BaseTest {
 		shop.dragAndDrop(shop.sliderInitialPosition, 100, 0); //am tras 100 pixeli din stanga la dreapta
 		shop.dragAndDrop(shop.sliderFinalPosition, -100, 0);  //am tras dinspre dreapta in stanga 100 pixeli
 		
+	}
+	
+	@Test
+	public void sendKeysExample() {
+		
+		MenuPage menu = new MenuPage(driver); //pt a putea naviga
+		menu.navigateTo(menu.contactsLink);
+		
+		ContactsPage contacs = new ContactsPage(driver);
+		WebElement nameElement = driver.findElement(contacs.name);
+		
+		Actions action = new Actions(driver);
+		
+		action
+		    .moveToElement(nameElement)
+		    .click(nameElement)
+		    .sendKeys("nume")
+			.sendKeys(Keys.TAB, "email@email.com")
+			.pause(Duration.ofSeconds(2))  //un fel de Thead sleep 2 sec
+			.sendKeys(Keys.TAB, "My subject")
+			.pause(Duration.ofSeconds(2))
+			.sendKeys(Keys.TAB, "My message")
+			.pause(Duration.ofSeconds(2))
+			.sendKeys(Keys.TAB, Keys.ENTER)
+			.pause(Duration.ofSeconds(2)) 
+			.perform();
 	}
 }
